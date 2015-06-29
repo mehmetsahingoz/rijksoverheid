df8cee99de72add10d2cda18e7761e4f0d757200
Oscar Farg <oscar@bosgeest.nl>
2015-06-29 16:39:15 +0200

    Added selenium-1 example

---
+++ selenium-1/src/test/java/nl/rijksoverheid/test/example/FirstExampleTest.java
@@ -0,0 +1,44 @@
+package nl.rijksoverheid.test.example;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+
+/**
+ * Created by oscar on 29-6-15.
+ */
+public class FirstExampleTest {
+
+    public static final String AMAZON = "Amazon";
+    public static final String FIRST_RESULT = "//*[@id=\"rhs_block\"]/ol/li/div[1]/div/div[1]/ol/li[2]/div/div[1]";
+    private WebDriver driver;
+
+    @Before
+    public void setUp() throws Exception {
+        driver = new FirefoxDriver();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        driver.quit();
+    }
+
+    @Test
+    public void testFarg() throws Exception {
+
+        driver.get("http://www.farg.nl");
+
+        WebElement element = driver.findElement(By.xpath("/html/body/img"));
+        assertEquals("img", element.getTagName());
+    }
+}
