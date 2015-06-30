23473746563ec791ec53ef793014da98958a20a1
Oscar Farg <oscar@bosgeest.nl>
2015-06-30 12:12:47 +0200

    added translator

---
+++ src/main/java/nl/rijksoverheid/test/junit/Translater.java
@@ -0,0 +1,30 @@
+package nl.rijksoverheid.test.junit;
+
+/**
+ * Created by oscar on 30-6-15.
+ */
+public class Translater {
+
+    private static final String[] WORDS_NL = {"fiets", "auto", "vliegtuig", "boot", "ruimteschip", "helicopter"};
+    private static final String[] WORDS_EN = {"bike", "car", "airplane", "boat", "spaceship", "helicopter"};
+
+    public String translateToEnglish(String dutchWord) {
+        for (int i = 0; i < WORDS_NL.length; i++) {
+            String word = WORDS_NL[i];
+            if (word.equals(dutchWord)) {
+                return WORDS_EN[i];
+            }
+        }
+        return null;
+    }
+
+    public String translateToDutch(String englishWord) {
+        for (int i = 0; i < WORDS_EN.length; i++) {
+            String word = WORDS_EN[i];
+            if (word.equals(englishWord)) {
+                return WORDS_NL[i];
+            }
+        }
+        return null;
+    }
+}
