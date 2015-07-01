eece643854d9a7bdc7492b73691dd23bcc559911
Oscar Farg <oscar@bosgeest.nl>
2015-07-01 11:55:53 +0200

    Added helloworldtest

--- src/test/java/nl/rijksoverheid/test/java/HelloWorldTest.java
+++ src/test/java/nl/rijksoverheid/test/java/HelloWorldTest.java
@@ -1,13 +1,24 @@
 package nl.rijksoverheid.test.java;
 
+import org.junit.Before;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 /**
  * Created by oscar on 1-7-15.
- *
- * JAVA EXERCISE 1:
- *
  */
 public class HelloWorldTest {
 
+    private HelloWorld helloWorld;
+
+    @Before
+    public void setUp() throws Exception {
+        this.helloWorld = new HelloWorld();
+    }
+
+    @Test
+    public void testHelloWorld() throws Exception {
+        assertEquals("Hello World", helloWorld.helloWorld());
+    }
 }
