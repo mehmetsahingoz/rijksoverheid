d7c513779b7d45cc4d0e6fa65b26f51dd9434398
Oscar Farg <oscar@bosgeest.nl>
2015-07-02 10:34:57 +0200

    Added exercise 2

--- src/test/java/nl/rijksoverheid/test/selenium/exercise2/SeleniumTest.java
+++ src/test/java/nl/rijksoverheid/test/selenium/exercise2/SeleniumTest.java
@@ -1,11 +1,30 @@
 package nl.rijksoverheid.test.selenium.exercise2;
 
 import nl.rijksoverheid.test.selenium.TestBase;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import static org.junit.Assert.*;
 
 /**
  * EXERCISE 2:
+ * Check is the E exists on http://www.rijksoverheid.nl/themas/internationale-en-ontwikkelingssamenwerking
  *
  * Step 1:
+ * Create a test method
+ *
+ * Step 2:
+ * Create an XPath query to select the E on the page.
+ *
+ * Step 3:
+ * Use selenium to check if the element exists on the page.
+ *
+ * Step 4:
+ * Check if the text of the element is E
+ *
+ * Step 5:
+ * Run the test.
  *
  * @author Oscar Farg
  *
